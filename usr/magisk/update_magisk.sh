c5080bc3f2efb847988388f8665c6421499cc3d2
GiovanYCringe <gio.pikagrossa@gmail.com>
2023-05-12 00:16:28 +0000

    Use old updater

--- usr/magisk/update_magisk.sh
+++ usr/magisk/update_magisk.sh
@@ -9,34 +9,34 @@ ver="$(cat "$DIR/magisk_version" 2>/dev/null || echo -n 'none')"
 if [ "x$1" = "xcanary" ]
 then
 	nver="canary"
-	magisk_link="https://github.com/topjohnwu/magisk-files/raw/${nver}/app-debug.apk"
-elif [ "x$1" = "xlocal" ]
-then
-	nver="local"
-	magisk_link="https://gitlab.com/TenSeventy7/magisk-files/raw/main/app-debug.apk"
+	magisk_link="https://github.com/topjohnwu/magisk_files/raw/${nver}/app-debug.apk"
 else
 	if [ "x$1" = "x" ]; then
-		nver="$(curl -s https://raw.githubusercontent.com/topjohnwu/magisk-files/master/stable.json | jq '.magisk.version' | cut -d '"' -f 2)"
+		nver="$(curl -s https://github.com/topjohnwu/Magisk/releases | grep -m 1 -Poe 'Magisk v[\d\.]+' | cut -d ' ' -f 2)"
 	else
 		nver="$1"
 	fi
-	magisk_link="https://cdn.jsdelivr.net/gh/topjohnwu/magisk-files@${nver}/app-release.apk"
+	magisk_link="https://github.com/topjohnwu/Magisk/releases/download/${nver}/Magisk-${nver}.apk"
 fi
 
-if [ \( -n "$nver" \) -a \( "$nver" != "$ver" \) -o ! \( -f "$DIR/arm/magiskinit64" \) -o \( "$nver" = "canary" \) -o \( "$nver" = "local" \) ]
+if [ \( -n "$nver" \) -a \( "$nver" != "$ver" \) -o ! \( -f "$DIR/arm/magiskinit64" \) -o \( "$nver" = "canary" \) ]
 then
 	echo "Updating Magisk from $ver to $nver"
 	curl -s --output "$DIR/magisk.zip" -L "$magisk_link"
 	if fgrep 'Not Found' "$DIR/magisk.zip"; then
 		curl -s --output "$DIR/magisk.zip" -L "${magisk_link%.apk}.zip"
 	fi
-
-	7z e "$DIR/magisk.zip" lib/arm64-v8a/libmagiskinit.so lib/armeabi-v7a/libmagisk32.so lib/arm64-v8a/libmagisk64.so -o"$DIR" -y
-	mv -f "$DIR/libmagiskinit.so" "$DIR/magiskinit"
-	mv -f "$DIR/libmagisk32.so" "$DIR/magisk32"
-	mv -f "$DIR/libmagisk64.so" "$DIR/magisk64"
-	xz --force --check=crc32 "$DIR/magisk32" "$DIR/magisk64"
-
+	if unzip -o "$DIR/magisk.zip" arm/magiskinit64 -d "$DIR"; then
+		mv -f "$DIR/arm/magiskinit64" "$DIR/magiskinit"
+		: > "$DIR/magisk32.xz"
+		: > "$DIR/magisk64.xz"
+	else
+		unzip -o "$DIR/magisk.zip" lib/arm64-v8a/libmagiskinit.so lib/armeabi-v7a/libmagisk32.so lib/arm64-v8a/libmagisk64.so -d "$DIR"
+		mv -f "$DIR/lib/arm64-v8a/libmagiskinit.so" "$DIR/magiskinit"
+		mv -f "$DIR/lib/armeabi-v7a/libmagisk32.so" "$DIR/magisk32"
+		mv -f "$DIR/lib/arm64-v8a/libmagisk64.so" "$DIR/magisk64"
+		xz --force --check=crc32 "$DIR/magisk32" "$DIR/magisk64"
+	fi
 	echo -n "$nver" > "$DIR/magisk_version"
 	rm "$DIR/magisk.zip"
 	touch "$DIR/initramfs_list"
